0e4b1a3d9d947894b263026612ac03934c2e8a4e
Zyix <bemonsterz01@gmail.com>
2026-06-04 11:13:24 +0300

    namespace update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,30 +1,21 @@
 <?php
-
 namespace Database\Seeders;
-
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Sanalkopru\Crm\Database\Seeders\CrmDealStageSeeder;
-use Sanalkopru\Crm\Database\Seeders\CrmDemoSeeder;
-use Sanalkopru\Crm\Database\Seeders\CrmPermissionSeeder;
+use App\Crm\Database\Seeders\CrmDealStageSeeder;
+use App\Crm\Database\Seeders\CrmDemoSeeder;
+use App\Crm\Database\Seeders\CrmPermissionSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call(CrmPermissionSeeder::class);
         $this->call(CrmDealStageSeeder::class);
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ])->assignRole('crm_owner');
-
         $this->call(CrmDemoSeeder::class);
     }
 }
